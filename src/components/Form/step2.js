64f17f7f9fd51a01326df5d3d488c000ef6f24ef
Rajesh Pillai <pillai.rajesh@gmail.com>
2019-05-13 22:54:04 +0530

    Nested 2

--- src/components/Form/step2.js
+++ src/components/Form/step2.js
@@ -1,16 +1,26 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import {
     Link,
 } from 'react-router-dom';
 
+import Context from '../../context';
+
 
 export default function Step2() {
+    let FormObject = useContext(Context);
+    console.log("Context: ", FormObject);
+    function onClick() {
+        FormObject.methods.update({
+            "step1": true
+        })
+    }
+
     return (
         <div>
             <h1>
                 Step 2
       </h1>
-            <Link to="/form/step3">Step 3</Link>
+            <Link onClick={() => onClick()} to="/form/step3">Step 3</Link>
         </div>
 
     )
